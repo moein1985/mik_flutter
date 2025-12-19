db1231acf21e452afa5e54c414007f3a5dae5320
moein1985 <moein1985@users.noreply.github.com>
2025-12-19 08:08:10 -0800

    before release
    Former-commit-id: a4874db6ae6d91ff4c7c74c666d2c6dedb696bc1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.hsmik"
+    namespace = "app.hsco.mik"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,8 +30,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.hsmik"
+        applicationId = "app.hsco.mik"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
